42c34eb6feae741de53c9a14fcd492336231564f
Kouissi Mohamed <mohamed.kouissi@gmail.com>
2025-12-12 14:17:28 +0100

    Fix typo in output message

--- maven/src/main/java/maven/Hello.java
+++ maven/src/main/java/maven/Hello.java
@@ -4,7 +4,7 @@ public class Hello {
 	
 	public static void main(String[] args)
     {
-        System.out.println("Hello DevOps, Today is Friday........");
+        System.out.println("Hello DevOps, Today is Friday.......");
     }
 
 }
